abd0ac71b296bf1744fd710e84dcfd17f10a4eda
cindyxinyiwang <xwang29@nd.edu>
2015-03-18 22:35:33 -0400

    form

---
+++ email.php
@@ -0,0 +1,17 @@
+<?php
+    $action=$_REQUEST['action'];
+    $name=$_REQUEST['name'];
+    $email=$_REQUEST['email'];
+    $message=$_REQUEST['message'];
+    if (($name=="")||($email=="")||($message==""))
+        {
+        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+        }
+    else{        
+        $from="From: $name<$email>\r\nReturn-path: $email";
+        $subject="Message sent using your contact form";
+        mail("xwang29@nd.edu", $subject, $message, $from);
+        echo "Email sent!";
+        }
+    }  
+?>
